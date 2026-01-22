e02e27dfcbb7ad6790f6b0743cdd335ff2d69cd6
droopy16a <pauldubois1122@gmail.com>
2026-01-22 20:53:37 +0100

    ahh

--- prout.sh
+++ prout.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-for (( i = 0; i < 9; i++ )); do
+for (( i = 0; i < 500; i++ )); do
         count="prout_$i"
         mkdir "$count"
         echo "bite" > "$count/$count.txt"
-
 
         chmod -w "$count"
 done
